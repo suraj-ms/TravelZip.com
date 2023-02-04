2a1f0bddaa63e68160f01169d9baeed214aa281a
suraj-ms <47383168+suraj-ms@users.noreply.github.com>
2023-02-04 16:42:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    // res.sendFile( path.join(__dirname+"/public/UI/index.html"))
+    res.sendFile(__dirname+'/public/UI/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
